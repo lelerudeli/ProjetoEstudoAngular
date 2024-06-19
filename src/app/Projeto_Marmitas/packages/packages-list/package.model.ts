fce9204a9c80f30852a857f9002ece85dc24c3ae
Letícia Rudeli <leticiarudeli11@gmail.com>
2024-06-19 15:56:56 -0300

    implementação da lista de marmita e dos detalhes de "cada" marmita

---
+++ src/app/Projeto_Marmitas/packages/packages-list/package.model.ts
@@ -0,0 +1,14 @@
+export class Package {
+
+    public name: string;
+    public desc: string;
+    public imagePath: string;
+
+    constructor(n: string, d: string, i: string){
+        this.name = n;
+        this.desc = d;
+        this.imagePath = i;
+
+
+    }
+}
--- src/app/Projeto_Marmitas/packages/packages-list/packages-list.component.ts
+++ src/app/Projeto_Marmitas/packages/packages-list/packages-list.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { PackagesItemComponent } from './packages-item/packages-item.component';
+import { Package } from './package.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-packages-list',
   standalone: true,
-  imports: [PackagesItemComponent],
+  imports: [PackagesItemComponent, CommonModule],
   templateUrl: './packages-list.component.html',
   styleUrl: './packages-list.component.css'
 })
 export class PackagesListComponent {
 
-  packages = [];
+  packages: Package[] = [
+    new Package('frango', 'filé de frango ao molho', 'https://vovopalmirinha.com.br/wp-content/uploads/2016/05/frango-molho.jpg'),
+    new Package('frango', 'filé de frango ao molho', 'https://vovopalmirinha.com.br/wp-content/uploads/2016/05/frango-molho.jpg')
+
+  ];
 }
