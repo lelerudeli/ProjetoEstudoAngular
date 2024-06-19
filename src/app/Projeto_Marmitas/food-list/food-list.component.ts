922b66dec5a37001da74d1b4ba71ee727207c7c2
Letícia Rudeli <leticiarudeli11@gmail.com>
2024-06-19 16:19:39 -0300

    terminei a parte de implementação de todos os componentes

--- src/app/Projeto_Marmitas/food-list/food-list.component.ts
+++ src/app/Projeto_Marmitas/food-list/food-list.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { FoodEditComponent } from './food-edit/food-edit.component';
+import { Food } from '../shared/food.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-food-list',
   standalone: true,
-  imports: [FoodEditComponent],
+  imports: [FoodEditComponent, CommonModule],
   templateUrl: './food-list.component.html',
   styleUrl: './food-list.component.css'
 })
 export class FoodListComponent {
+
+  foodList: Food[]= [
+    new Food('frango', 1),
+    new Food('arroz', 2),
+    new Food('molho', 3)
+  ];
 
 }
