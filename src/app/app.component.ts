dbc84ec70987c7c469eb5c4d633fb6700e8cfe96
Letícia Rudeli <leticiarudeli11@gmail.com>
2024-06-20 15:00:00 -0300

    seleção do que será mostrado pelo header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Projeto_Marmitas/header/header.component';
 import { PackagesComponent } from './Projeto_Marmitas/packages/packages.component';
 import { FoodListComponent } from './Projeto_Marmitas/food-list/food-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PackagesComponent, FoodListComponent],
+  imports: [RouterOutlet, 
+    HeaderComponent, 
+    PackagesComponent, 
+    FoodListComponent, 
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  title = 'oi'
+  loadedFeature = 'packages';
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
   
 }
